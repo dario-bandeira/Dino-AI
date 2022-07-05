0ae68e2ace522fe3e68822ebb8fa840f3eb996f0
Dario Bandeira <dario.b.santos@gmail.com>
2022-07-05 13:27:29 -0400

    retirado o score padrão

--- main_2022.py
+++ main_2022.py
@@ -15,8 +15,6 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 background_col = (235, 235, 235)
 
-high_score = 0
-
 screen = pygame.display.set_mode(scr_size)
 clock = pygame.time.Clock()
 pygame.display.set_caption("T-Rex Rush")
@@ -252,42 +250,13 @@ class Cloud(pygame.sprite.Sprite):
 			self.kill()
 
 
-class Scoreboard:
-	def __init__(self, x=-1, y=-1):
-		self.score = 0
-		self.tempimages, self.temprect = load_sprite_sheet('numbers.png', 12, 1, 11, int(11 * 6 / 5), -1)
-		self.image = pygame.Surface((55, int(11 * 6 / 5)))
-		self.rect = self.image.get_rect()
-		if x == -1:
-			self.rect.left = width * 0.89
-		else:
-			self.rect.left = x
-		if y == -1:
-			self.rect.top = height * 0.1
-		else:
-			self.rect.top = y
-
-	def draw(self):
-		screen.blit(self.image, self.rect)
-
-	def update(self, score):
-		score_digits = extract_digits(score)
-		self.image.fill(background_col)
-		for s in score_digits:
-			self.image.blit(self.tempimages[s], self.temprect)
-			self.temprect.left += self.temprect.width
-		self.temprect.left = 0
-
-
 def gameplay():
-	global high_score, FPS
+	global FPS
 	gamespeed = 4
 	gameover = False
 	gamequit = False
 	playerdino = Dino(44, 47)
 	new_ground = Ground(-1 * gamespeed)
-	scb = Scoreboard()
-	highsc = Scoreboard(width * 0.78)
 	counter = 0
 
 	cacti = pygame.sprite.Group()
@@ -370,17 +339,11 @@ def gameplay():
 		pteras.update()
 		clouds.update()
 		new_ground.update()
-		scb.update(playerdino.score)
-		highsc.update(high_score)
 
 		if pygame.display.get_surface() is not None:
 			screen.fill(background_col)
 			new_ground.draw()
 			clouds.draw(screen)
-			scb.draw()
-			if high_score != 0:
-				highsc.draw()
-				screen.blit(hi_image, hi_rect)
 			cacti.draw(screen)
 			pteras.draw(screen)
 			playerdino.draw()
@@ -412,8 +375,6 @@ def gameplay():
 
 		if playerdino.isDead:
 			gameover = True
-			if playerdino.score > high_score:
-				high_score = playerdino.score
 
 		if counter % 700 == 699:
 			new_ground.speed -= 1
